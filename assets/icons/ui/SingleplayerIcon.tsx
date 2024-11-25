7b9c2566e65da19f9dcf57747467423f66f42f99
agustingomezdev <agustingomezdev@gmail.com>
2024-11-25 16:30:01 +0100

    upload ui icons

---
+++ assets/icons/ui/SingleplayerIcon.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function SingleplayerIcon(props: SVGProps<SVGSVGElement>) {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><g fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth={4}><path d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"></path><path d="M20 24v-6.928l6 3.464L32 24l-6 3.464l-6 3.464z"></path></g></svg>);
+}
