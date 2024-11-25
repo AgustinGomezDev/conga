e3af76e96dc6a0dd435602b0147302c1b99f931c
agustingomezdev <agustingomezdev@gmail.com>
2024-11-25 17:41:03 +0100

    fix responsive desktop

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Bento from '@/components/Bento'
 
 const page = () => {
     return (
-        <main className='bg-background container mx-auto px-64'>
+        <main className='bg-background container mx-auto px-10 md:px-20 lg:px-52 xl:px-64'>
             <h1 className="text-7xl font-bold py-8">La Conga</h1>
             <Bento />
         </main>
