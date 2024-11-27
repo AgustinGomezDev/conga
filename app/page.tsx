df023ecd2cad6e1a3940020c404bdc981c4def15
agustingomezdev <agustingomezdev@gmail.com>
2024-11-27 13:33:33 +0100

    add Settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Bento from '@/components/Bento'
-import { ThemeToggler } from '@/components/ThemeToggler'
+import Settings from '@/components/Settings'
 
 const page = () => {
     return (
         <main className='bg-background container mx-auto px-10 md:px-20 lg:px-52 xl:px-64'>
             <div className='flex justify-between items-center gap-5'>
                 <h1 className="text-7xl font-bold py-8">La Conga</h1>
-                <ThemeToggler />
+                <Settings />
             </div>
             <Bento />
         </main>
---
+++ components/Settings.tsx
@@ -0,0 +1,43 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogClose
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { ThemeToggler } from "./ThemeToggler"
+import { SettingsIcon } from "@/assets/icons/ui/SettingsIcon"
+
+const Settings = () => {
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <SettingsIcon className="size-6" />
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="font-bold text-xl">Configuración</DialogTitle>
+                    {/* <DialogDescription className="text-md text-muted-foreground">Personaliza la experiencia a tu gusto ajustando las opciones disponibles.</DialogDescription> */}
+                </DialogHeader>
+                <div>
+                    <div>
+                        <p className="text-lg font-semibold text-foreground">Temas</p>
+                        <span className="text-sm text-muted-foreground mb-4 block">Cambia entre opciones claras u oscuras para mejorar la apariencia y la comodidad al usar la aplicación.</span>
+                        <ThemeToggler />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button >Cerrar</Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default Settings
